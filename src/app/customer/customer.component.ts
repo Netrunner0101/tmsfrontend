2f67467d7aa651e08f4e3889c169089bf7282fa3
Netrunner0101 <ericlung6572@gmail.com>
2022-08-03 04:01:20 +0200

    finit excel et pdf

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CustomerserviceService } from '../service/customerservice.service';
-
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-customer',
@@ -31,6 +31,25 @@ export class CustomerComponent implements OnInit {
   // If Problem look at back end because could be at the response Ok
   deleteCustomer(idc:any){
     this._customerService.deleteCustomer(idc);
+  }
+
+  exportExcel() {
+    if (this.Customers.length > 0) {
+      import("xlsx").then(xlsx => {
+        const worksheet = xlsx.utils.json_to_sheet(this.Customers);
+        const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+        this.saveAsExcelFile(excelBuffer, "customers");
+      });
+    }
+  }
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    let EXCEL_EXTENSION = '.xlsx';
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+    });
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
   }
 
 }
--- src/app/transporter/transporter.component.ts
+++ src/app/transporter/transporter.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Transporter } from '../model/transporter.model';
 import { TransporterService } from '../service/transporter.service';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-transporter',
@@ -31,6 +32,25 @@ export class TransporterComponent implements OnInit {
         console.log("Delete Transporter sucess");
       }
     )
+  }
+
+  exportExcel() {
+    if (this.Transporters.length > 0) {
+      import("xlsx").then(xlsx => {
+        const worksheet = xlsx.utils.json_to_sheet(this.Transporters);
+        const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+        this.saveAsExcelFile(excelBuffer, "transporter");
+      });
+    }
+  }
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    let EXCEL_EXTENSION = '.xlsx';
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+    });
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
   }
 
 }
